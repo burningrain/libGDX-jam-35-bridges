a0d274977c67b9a9f50ad0d72a02a2d1f4a33dfd
ash <lixkram@gmail.com>
2025-12-11 01:03:58 +0300

    - fix the jump logic. A jump has priority above then a step

--- core/src/main/java/com/github/br/libgdx/jam35/model/StepResolver.java
+++ core/src/main/java/com/github/br/libgdx/jam35/model/StepResolver.java
@@ -17,40 +17,45 @@ public class StepResolver {
         int currentX = currentCell.getX();
         int currentY = currentCell.getY();
 
-        // налево
-        addStepIfPossible(currentX - 1, currentY, grid, currentCell, result);
-        addJumpIfPossible(currentX - 2, currentY, grid, currentCell, result);
-        // направо
-        addStepIfPossible(currentX + 1, currentY, grid, currentCell, result);
-        addJumpIfPossible(currentX + 2, currentY, grid, currentCell, result);
-        // вверх
-        addStepIfPossible(currentX, currentY + 1, grid, currentCell, result);
-        addJumpIfPossible(currentX, currentY + 2, grid, currentCell, result);
-        // вниз
-        addStepIfPossible(currentX, currentY - 1, grid, currentCell, result);
-        addJumpIfPossible(currentX, currentY - 2, grid, currentCell, result);
+        // бить обязательно. бой имеет приоритет над ходом
+        boolean isJumpLeft = addJumpIfPossible(currentX - 2, currentY, grid, currentCell, result); // налево
+        boolean isJumpRight = addJumpIfPossible(currentX + 2, currentY, grid, currentCell, result); // направо
+        boolean isJumpUp = addJumpIfPossible(currentX, currentY + 2, grid, currentCell, result); // вверх
+        boolean isJumpDown = addJumpIfPossible(currentX, currentY - 2, grid, currentCell, result); // вниз
+        if (isJumpLeft || isJumpRight || isJumpUp || isJumpDown) {
+            return result;
+        }
+
+        addStepIfPossible(currentX - 1, currentY, grid, currentCell, result); // налево
+        addStepIfPossible(currentX + 1, currentY, grid, currentCell, result); // направо
+        addStepIfPossible(currentX, currentY + 1, grid, currentCell, result); // вверх
+        addStepIfPossible(currentX, currentY - 1, grid, currentCell, result); // вниз
 
         return result;
     }
 
-    private void addStepIfPossible(int x, int y, Grid grid,
-                                   Cell currentCell, Array<Cell> result) {
+    private boolean addStepIfPossible(int x, int y, Grid grid,
+                                      Cell currentCell, Array<Cell> result) {
         if (!validator.isAbleToStep(grid, currentCell, x, y)) {
-            return;
+            return false;
         }
 
         Cell[][] cells = grid.getGrid();
         result.add(cells[x][y]);
+
+        return true;
     }
 
-    private void addJumpIfPossible(int x, int y, Grid grid,
-                                   Cell currentCell, Array<Cell> result) {
+    private boolean addJumpIfPossible(int x, int y, Grid grid,
+                                      Cell currentCell, Array<Cell> result) {
         if (!validator.isAbleToJump(grid, currentCell, x, y)) {
-            return;
+            return false;
         }
 
         Cell[][] cells = grid.getGrid();
         result.add(cells[x][y]);
+
+        return true;
     }
 
 }
